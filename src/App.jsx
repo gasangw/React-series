6248cb56239ddc042732298208b024ade225521d
gasangw <thomasgasangwa@gmail.com>
2023-09-26 08:24:01 +0200

    Change the background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import HelloWorld from "./Components/HelloWorld"
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl text-center text-green-600">React Projects</h1>
-    </>
+    <div>
+      <HelloWorld />
+    </div>
   )
 }
 
---
+++ src/Components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HelloWorld() {
+  return (
+    <div className='h-[100vh] bg-yellow-400 text-center grid my-auto place-content-center'>
+        <p className='text-xl font-medium'>Hello, World!</p>
+    </div>
+  )
+}
+
+export default HelloWorld
