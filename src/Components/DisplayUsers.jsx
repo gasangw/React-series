338b450f8753fc6432bc1af2eef6cdd8aa6bdb99
gasangw <thomasgasangwa@gmail.com>
2023-09-28 17:39:39 +0200

    Add styles for the flip card

--- src/Components/DisplayUsers.jsx
+++ src/Components/DisplayUsers.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
-function DisplayUsers() {
+function DisplayUsers(props) {
   return (
-    <div>DisplayUsers</div>
-  )
+    <div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img
+              src="img_avatar.png"
+              alt="Avatar"
+              style={{width:'300px', height:'300px'}}
+            />
+          </div>
+          <div className="flip-card-back">
+            <h1>John Doe</h1>
+            <p>Architect & Engineer</p>
+            <p>We love that guy</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DisplayUsers
+export default DisplayUsers;
