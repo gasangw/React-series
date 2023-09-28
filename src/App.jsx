04a097afc36be067b4a29857f46f745eb9cf7cb7
gasangw <thomasgasangwa@gmail.com>
2023-09-28 16:04:46 +0200

    Solve the list rendering challenge 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import DisplayList from "./Components/DisplayList"
+
 function App() {
-
-  return (
-    <>
-  
-    </>
-  )
-}
-
+  let data = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
+   return (
+     <div className="grid justify-center align-middle mx-auto mt-9">
+      {data.map(animal => <DisplayList animal={animal}/>)}
+     </div>
+   )
+ }
 export default App
