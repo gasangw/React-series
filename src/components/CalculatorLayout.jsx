381bd38d6ede80da206a0d77e12e85d7dec72169
gasangw <thomasgasangwa@gmail.com>
2023-09-29 07:40:54 +0200

    Complete styling of the inputs

--- src/components/CalculatorLayout.jsx
+++ src/components/CalculatorLayout.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function CalculatorLayout() {
   return (
     <div className='grid mt-12'>
-       <div className='grid bg-slate-200 mx-auto w-2/5 h-96 border border-red-500'>
+       <div className='grid bg-slate-200 mx-auto w-2/5 h-96'>
           <button className='text-xl font-semibold bg-gray-400 py-2'>1</button>
           <div className='grid grid-cols-4'>
           <input
@@ -15,98 +15,81 @@ function CalculatorLayout() {
             <input
                 type="button"
                 value="+/-"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="%"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="&#247;"
-                className="button-operator"
-                id="red"
+                className="bg-orange-500 text-white"
             />
             <input
                 type="button"
                 value="7"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="8"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="9"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="x"
-                className="button-operator"
-                id="red"
+                className="bg-orange-500 text-white"
             />
             <input
                 type="button"
                 value="4"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="5"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="6"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="-"
-                className="button-operator"
-                id="red"
+                className="bg-orange-500 text-white"
             />
             <input
                 type="button"
                 value="1"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="2"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="3"
-                className="button-operator"
             />
             <input
                 type="button"
                 value="+"
-                className="button-operator"
-                id="red"
+                className="bg-orange-500 text-white"
             />
 
             <input
                 type="button"
                 value="0"
-                className="button-operator"
+                className='grid col-span-2'
             />
             <input
                 type="button"
                 value="."
-                className="button-operator"
             />
             <input
                 type="button"
                 value="="
-                className="button-operator"
-                id="red"
+                className="bg-orange-500 text-white"
             />
           </div>
        </div>
