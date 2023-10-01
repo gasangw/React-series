a31504bd26d0bbf04ebdee84a76804d5608695a5
gasangw <thomasgasangwa@gmail.com>
2023-10-01 19:22:41 +0200

    Add double quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import DisplayList from "./Components/DisplayList"
 
 function App() {
-  let data = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
+  let data = ["dog", "cat", "chicken", "cow", "sheep", "horse"]
    return (
      <div className="grid justify-center align-middle mx-auto mt-9">
-      {data.map(animal => <DisplayList animal={animal}/>)}
+      {data.map((animal, index) => <DisplayList animal={animal} key={index}/>)}
      </div>
    )
  }
