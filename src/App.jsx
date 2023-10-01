9fb8aca6833950fae0f44cf95a5cb8abd84f39ae
gasangw <thomasgasangwa@gmail.com>
2023-10-01 19:16:00 +0200

    Add key on each child

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ function App() {
  let data = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
   return (
     <div className="grid justify-center align-middle mx-auto mt-9">
-     {data.map(animal => {
+     {data.map((animal, index) => {
       return (
-        <ul>
+        <ul key={index}>
            <li className='text-base list-disc'>{animal}</li>
         </ul>
       )
