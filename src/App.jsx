c7fa4748e92dd6ac674f7c4c828b826da4eba072
gasangw <thomasgasangwa@gmail.com>
2023-10-01 16:48:47 +0200

    Add the navlinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Navbar from './Components/Navbar'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
-  
+      <Routes>
+        <Route path="/about" element={<Navbar />} />
+      </Routes>
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div className='bg-[#FFF7ED] flex justify-between px-16 py-8'>
+        <div>
+           <NavLink to='/' className='text-black font-bold hover:font-bold hover:underline'>#VANLIFE</NavLink>
+        </div>
+        <nav className='flex gap-3'>
+           <NavLink to='/' className="hover:font-bold hover:underline">About</NavLink>
+           <NavLink to='/vans' className="hover:font-bold hover:underline">Vans</NavLink>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
