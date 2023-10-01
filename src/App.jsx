849258ed221bc71f02dd62b2474b8d4dca451fcc
gasangw <thomasgasangwa@gmail.com>
2023-10-01 18:11:46 +0200

    Set up all the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from './Components/Navbar'
+import About from './Components/About'
+import Vans from './Components/Vans'
+import Home from './Components/Home'
 import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
+     <Navbar />
       <Routes>
-        <Route path="/about" element={<Navbar />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,8 @@ function Navbar() {
            <NavLink to='/' className='text-black font-bold hover:font-bold hover:underline'>#VANLIFE</NavLink>
         </div>
         <nav className='flex gap-3'>
-           <NavLink to='/' className="hover:font-bold hover:underline">About</NavLink>
+           <NavLink to='/' className="hover:font-bold hover:underline">Home</NavLink>
+           <NavLink to='/about' className="hover:font-bold hover:underline">About</NavLink>
            <NavLink to='/vans' className="hover:font-bold hover:underline">Vans</NavLink>
         </nav>
     </div>
