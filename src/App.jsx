41460cf8af83ebb8a35dcd12b946268727dd595f
gasangw <thomasgasangwa@gmail.com>
2023-10-03 11:59:24 +0200

    Add double quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
- let data = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse'];
+ let data = ["dog", "cat", "chicken", "cow", "sheep", "horse"];
 
- let myAnimals = data.map((animal, index) => (<li className='text-base list-disc' key={index}>{animal}</li>))
+ let myAnimals = data.map((animal, index) => (<li className="text-base list-disc" key={index}>{animal}</li>))
  
   return (
     <div className="grid justify-center align-middle mx-auto mt-9">
