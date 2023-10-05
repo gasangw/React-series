b9de8ac29fa4ca5a30235ea87cedb83393eca6e4
gasangw <thomasgasangwa@gmail.com>
2023-10-05 16:02:07 +0200

    Add the unqiue key

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import uuid from 'react-uuid';
 function App() {
  let data = ["dog", "cat", "chicken", "cow", "sheep", "horse"];
 
- let myAnimals = data.map((animal, index) => (<li className="text-base list-disc" key={index}>{animal}</li>))
+ let myAnimals = data.map(animal => (<li className="text-base list-disc" key={uuid()}>{animal}</li>))
  
   return (
     <div className="grid justify-center align-middle mx-auto mt-9">
