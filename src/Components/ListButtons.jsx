e3be3106c1f169c4c77f202b46c0f55875d866cb
gasangw <thomasgasangwa@gmail.com>
2023-10-05 19:36:47 +0200

    Add the navbar

--- src/Components/ListButtons.jsx
+++ src/Components/ListButtons.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-
 function DisplayButtons(props) {
-  console.log(props.data)
     const { id, button} = props.data
-
+  
     const hanldeClick =(e)=>{
         alert(`You Clicked on Button ${e.currentTarget.id}`)
     }
   return (
-    <div className='flex gap-3 items-center my-auto'>
-       <button id={id} className='border border-gray-300 rounded-md p-3 hover:bg-green-400 hover:text-white' onClick={hanldeClick}>{button}</button>
+    <div className="flex gap-3 items-center my-auto">
+      <button id={id} className="border border-gray-300 rounded-md p-3 hover:bg-green-400 hover:text-white" onClick={hanldeClick}>{button}</button>
     </div>
   )
 }
 
 export default DisplayButtons
+
+// 
+// return (
+//   <div className="grid place-content-center my-auto h-[100vh]">
+//     <div className="flex gap-1">
--- src/Components/MapAllButtons.jsx
+++ src/Components/MapAllButtons.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
 import DisplayButtons from './ListButtons'
 import BUTTON_CONTENTS from './ButtonData'
+import uuid from "react-uuid";
 
 function MyButton() {
+  const topNav = ["Home", "exercise 1", "exercise 2", "exercise 3", "exercise 4","exercise 5", "exercise 6", "exercise 7", "exercise 8", "exercise 9"]
+ 
   return (
-    <div className='flex gap-4 justify-center items-center my-auto h-[100vh]'>
-       { BUTTON_CONTENTS.map((eachButton)=> {
-        return (
-            <div key={eachButton.id}>
-                <DisplayButtons data={eachButton}/>
-            </div>
-        )
-       })} 
+<div className='grid'>
+  <div className='flex mx-auto mt-16'>
+    {topNav.map(element => {
+      return (
+      <ul key={uuid()}>
+          <li className="underline pr-1 border-r-2 border-black">{element}</li>
+      </ul>
+    )})}
     </div>
+    <div className='flex gap-4 justify-center items-center my-auto h-[100vh]'>
+        { BUTTON_CONTENTS.map((eachButton)=> {
+          return (
+              <div key={eachButton.id}>
+                  <DisplayButtons data={eachButton}/>
+              </div>
+          )
+        })} 
+      </div>
+</div>
   )
 }
 
